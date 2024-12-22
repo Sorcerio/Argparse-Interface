ab0b37a1429ffd6979ac0adf65953d7ede33b008
Brody Childs <maximombro@gmail.com>
2024-12-22 14:17:16 -0500

    Updated setup script

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="argparse_interface",
+    name="argui",
     version="0.1.0",
-    packages=find_packages(where="argui"),
-    package_dir={"": "argui"},
+    packages=find_packages(),
     install_requires=open("requirements.txt").read().splitlines(),
     entry_points={
         "console_scripts": [
-            "arguidemo=argui.Demo:demoCli"
+            "arguidemo=argui.Demo:runDemo"
         ]
     },
     include_package_data=True,
